cc6dab2635f5974ac87054b48b076031239e251e
Jefferson1994 <jeffersonk132008@hotmail.com>
2025-09-16 09:11:58 -0500

    entornos

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000' // Tu URL local
+};
+/*export const environment = {
+  production: true,
+  apiUrl: 'https://appcontrolbarberias.onrender.com' // Tu URL del servidor
+};*/
